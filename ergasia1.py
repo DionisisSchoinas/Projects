5eaa61a956a721dd886c39d2b5fcb3272a42deb0
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-01-10 16:33:41 +0200

    ergasia #1

--- ergasia1.py
+++ ergasia1.py
@@ -3,9 +3,11 @@ import random
 import time
 
 start_time = time.time()
-def getNumbers(numbers,a):
+def getNumbers(a):
+	numbers=[]
 	for i in range(100):
-		numbers.append(random.sample(a,5))		
+		numbers.append(random.sample(a,5))	
+	return numbers
 def callNumbers(choice,a):
 	k=[0]*80
 	exit=True
@@ -15,7 +17,7 @@ def callNumbers(choice,a):
 		x=random.sample(a,1)
 		a.remove(x[0])
 		for i in range(0,80):
-			if (x[0] in choice[i]):
+			if x[0] in choice[i]:
 				k[i]=k[i]+1
 				if k[i]==5:
 					exit=False
@@ -24,8 +26,7 @@ def callNumbers(choice,a):
 bingos=0
 for i in range(1000):
 	a=range(1,81)
-	players=[]
-	getNumbers(players,a)
+	players=getNumbers(a)
 	bingos=bingos+callNumbers(players,a)
 print bingos/1000
 print time.time() - start_time
