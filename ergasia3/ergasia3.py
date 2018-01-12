d7f2d82421347788f0cc6b419e993bff73d9f3af
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-01-12 15:05:11 +0200

    ergasia #3

--- ergasia3/ergasia3.py
+++ ergasia3/ergasia3.py
@@ -1,25 +1,25 @@
+
 in_text=raw_input("Give the file you want to encode (.txt)\n")
 try:
 	text = open(in_text,"r")
 	chars = []
-	k=0
-	for li in text:
-		for c in li:
-			k=k+1
-			chars.append(c)
+	i=0
 	asci = range(ord('A'),ord('Z')+1)+range(ord('a'),ord('z')+1)
-	for i in range(k):
-		x = ord(chars[i])
-		if x in asci:
-			leto = asci.index(x)
-			if leto<39:
-				letn = leto+13			
-			else:
-				letn = -(51-leto)+13
-			value = asci[letn]
-			chars[i] = chr(value)
+	for li in text:
+		for c in li:			
+			chars.append(c)
+			x = ord(chars[i])
+			if x in asci:
+				leto = asci.index(x)
+				if leto<39:
+					letn = leto+13			
+				else:
+					letn = -(51-leto)+13
+				value = asci[letn]
+				chars[i] = chr(value)
+			i=i+1
 	output = ""
-	output = output.join([str(chars[i]) for i in range(k)])
+	output = output.join([str(chars[k]) for k in range(i)])
 	out_text=raw_input("Give the name of the file you want to save the encoded message (.txt)\n")
 	enc = open(out_text,"w")
 	enc.write(output)
