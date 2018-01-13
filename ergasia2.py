1a141d85916d1e7846f9d615d07bdece8e8b5a2d
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-01-13 23:50:55 +0200

    ergasia #2
    Get time every 5 min for every day

--- ergasia2.py
+++ ergasia2.py
@@ -2,25 +2,53 @@ import urllib2
 import json
 import datetime
 
-cur_date =datetime.datetime.now()
-month = cur_date.strftime("%m-%Y")
-now = cur_date.strftime("%d-%m-%Y")
-url = "http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json"%now
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
-data = response.read()
-data = json.loads(data)
+picks = [""]*10
+for i in range(10):
+	exit = True
+	while exit:
+		x = i+1
+		#x = raw_input("Give number %s : "%(i+1))
+		try:
+			x = int(x)
+			if x in range(1,81) and not(x in picks):
+				exit = False
+				picks[i] = x
+			else:
+				print "Give a number between 1-80 and select each number ones"
+		except:
+			print "Give a number between 1-80"
+n = datetime.datetime.now()
+a = datetime.datetime(n.year, n.month, 1, 9, 00)
+c = datetime.datetime(n.year, n.month, n.day , 23, n.minute-n.minute%5)
+b = datetime.datetime(n.year, n.month, n.day)	
 f = []
-exit = True
-i = 0
-while exit:
-	times = datadata['draws']['draw'][i]['drawTime']
-	x = data['draws']['draw'][i]['results']
-	y = list(x)
-	f.append(x)
-	i+=1
-	if not(y[0]=='1' and y[1]=='2'):
-		print "in"
-		exit = False
-for x in range(i):
-	print f[x]
+exit1 = True
+while exit1:		
+	i = 0
+	exit2 = True
+	d = datetime.datetime(n.year, n.month, b.day, 23, 55)
+	now = b.strftime("%d-%m-%Y")
+	url = "http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json"%now
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	data = response.read()
+	data = json.loads(data)
+	while exit2:	
+		exit3 = True
+		while exit3:			
+			x = data['draws']['draw'][i]['results']
+			#f.append(x)
+			y = data['draws']['draw'][i]['drawTime']
+			y = datetime.datetime.strptime(y, '%d-%m-%YT%H:%M:%S')
+			print i , y
+			i += 1
+			if y<a or y>=c or y>=d:
+				print "3"
+				exit3 = False	
+		if y<a or y>=c or y>=d:
+			print "2"
+			exit2 = False
+	if y<a:
+		print "1"
+		exit1 = False
+	b = b - datetime.timedelta(days=1)
