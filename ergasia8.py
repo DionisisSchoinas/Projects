9fd8fb5c901853b60d3ff54b2b932eb35993b005
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-01-13 21:00:27 +0200

    ergasia #8
    Added functions

--- ergasia8.py
+++ ergasia8.py
@@ -1,22 +1,29 @@
 import random
 import time
 
+def findCombos(comb):
+	f = 0
+	num = random.sample(range(-29,30),30)
+	for i in range(30):
+		x = num[i]
+		for j in range(i+1,30):
+			y = num[j]
+			for k in range(j+1,30):
+				if -x == y+num[k] :
+					f = f+1
+					comb.append([x,y,num[k]])
+	return f
+def Print(comb,f):
+	if comb == []:
+		print "Den uparxoun triades"
+	else:
+		print "bhka"
+		print f
+		for i in range(f):
+			print comb[i]
+			
 start_time = time.time()
-num = random.sample(range(-29,30),30)
 comb = []
-f = 0
-print num
-for i in range(30):
-	x = num[i]
-	for j in range(i+1,30):
-		y = num[j]
-		for k in range(j+1,30):
-			if -x == y+num[k] :
-				f = f+1
-				comb.append([x,y,num[k]])
-if comb == []:
-	print "Den uparxoun triades"
-else:
-	for i in range(f):
-		print comb[i]
+f = findCombos(comb)
+Print(comb,f)
 print time.time() - start_time
