d85f65f77904c4e4b540c83a6e96a6fac8583dd4
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-01-20 16:11:59 +0200

    ergasia #3
    Added function

--- ergasia3.py
+++ ergasia3.py
@@ -1,29 +1,31 @@
+def Change_Char(c,i,asci,chars):
+	b=False			
+	chars.append(c)
+	x = ord(chars[i])
+	if x in asci[0]:
+		a=list(asci[0])
+		b=True
+	elif x in asci[1]:
+		a=list(asci[1])
+		b=True
+	if b:
+		leto = a.index(x)
+		if leto<13:
+			letn = leto+13			
+		else:
+			letn = -(26-leto)+13
+		value = a[letn]
+		chars[i] = chr(value)
 
-in_text=raw_input("Give the file you want to encode (.txt)\n")
 try:
+	in_text=raw_input("Give the file you want to encode (.txt)\n")
 	text = open(in_text,"r")
 	chars = []
 	i=0
 	asci = [range(ord('A'),ord('Z')+1),range(ord('a'),ord('z')+1)]
 	for li in text:
 		for c in li:
-			b=False			
-			chars.append(c)
-			x = ord(chars[i])
-			if x in asci[0]:
-				a=list(asci[0])
-				b=True
-			elif x in asci[1]:
-				a=list(asci[1])
-				b=True
-			if b:
-				leto = a.index(x)
-				if leto<13:
-					letn = leto+13			
-				else:
-					letn = -(26-leto)+13
-				value = a[letn]
-				chars[i] = chr(value)
+			Change_Char(c,i,asci,chars)
 			i=i+1
 	output = ""
 	output = output.join([str(chars[k]) for k in range(i)])
@@ -34,4 +36,4 @@ try:
 	enc.close()
 	print "Text encoded"
 except:
-	print "File not found"
+	print "An error occuried"
